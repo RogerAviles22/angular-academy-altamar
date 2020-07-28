176809177762ab688fc137dce0554c913983aba8
RogerAviles22 <rogiavil@espol.edu.ec>
2020-07-28 16:19:20 -0500

    pequeño avance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, FontAwesomeModule ],
-  declarations: [ AppComponent, HelloComponent, NavbarComponent, FooterComponent ],
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, NavbarComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
