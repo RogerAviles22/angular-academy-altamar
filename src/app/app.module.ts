94b96f77e43d48e067588bc133ec196b6e00d8b7
RogerAviles22 <rogiavil@espol.edu.ec>
2020-07-29 10:47:09 -0500

    Agg Router link index y nosotros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { IndexComponent } from './secondary/index/index.component';
+import { NosotrosComponent } from './secondary/nosotros/nosotros.component';
+
+const rutas: Routes = [
+ { path: '', component: IndexComponent },
+ { path: 'nosotros', component: NosotrosComponent}
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, NavbarComponent, FooterComponent, IndexComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(rutas) ],
+  declarations: [ AppComponent, NavbarComponent, FooterComponent, IndexComponent, NosotrosComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
